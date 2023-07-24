3f855b62b780c3162172fcc8eb65ec3ff6420985
AYC93 <ayc.leo@gmail.com>
2023-07-25 06:45:30 +0800

    minor edits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -65,11 +65,11 @@ const appRoutes: Routes = [
     StoreModule.forFeature('login', loginReducer),
     StoreModule.forFeature('dashboard', dashboardReducer),
     StoreDevtoolsModule.instrument({
-      maxAge: 5, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      maxAge: 5, 
+      logOnly: !isDevMode(),
+      autoPause: true, 
+      trace: false, 
+      traceLimit: 75,
     }),
   ],
   providers: [ApiService, PlannerService, GoogleApiService, AuthGuard, LocalStorageService],
